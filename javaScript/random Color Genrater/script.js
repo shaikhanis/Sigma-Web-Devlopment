9055dad98560cb61a3b6b9da374b05e87887e491
shaikhanis <anisshaikh19059@gmail.com>
2024-01-23 15:29:51 +0530

    start with javaScript

---
+++ javaScript/random Color Genrater/script.js
@@ -0,0 +1,28 @@
+let boxes = document.querySelector(".container").children
+
+
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomColor()
+    e.style.color = getRandomColor()
+    // e.textContent = e.style.color;
+})
+
+// let boxes= document.querySelectorAll('.box')
+//         function changeBgColor(){
+//             let ran= Math.ceil(Math.random()*600000);
+//             ranplus= '#'+ran
+//             return ranplus
+
+//         }
+//         changeBgColor()
+//         boxes.forEach((e)=>{
+//             e.style.backgroundColor=changeBgColor()
+//             e.style.color=changeBgColor()
+//         })
+
